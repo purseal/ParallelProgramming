9d73c17bfa7c81116a21393a24376189be2b2623
Maria <korevmaria@gmail.com>
2015-10-30 16:02:37 +0300

    Second example

--- fork-count.py
+++ fork-count.py
@@ -3,8 +3,23 @@
 '''
     In this program I'm trying to use parallel programming, using examples
     in Mark Lutz Learning Python.
-    Ответвляет дочерние процессы, пока не будет нажата клавиша q.
+    Запускает 5 копий себя самого, при этом каждая копия считает 5 с
+    односекундной задержкой.
 '''
 
-import os
+import os, time
 
+def counter(count):
+    for i in range(count):
+        time.sleep(1)
+        print('[%s] => %s' % (os.getpid(), i))
+
+if __name__== '__main__':
+    for i in range(5):
+        pid = os.fork()
+        if pid != 0:
+            print('Process %d spawned' % pid)
+        else:
+            counter(5)
+            os._exit(0)
+    print('Main process exiting')
